fcc77a6bbd656b2ba06f710b1a00a2b40b86f15e
si14444 <a1@1ui-MacBookAir.local>
2023-12-28 05:16:22 +0900

    Layout/Header 작업

---
+++ src/app/_components/layout/Layout.tsx
@@ -0,0 +1,23 @@
+// Layout.js
+
+import React, { ReactNode } from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      <div className="flex">
+        <Sidebar />
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;

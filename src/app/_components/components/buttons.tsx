22ef265889e165c2532381c9cc180b9af3aec04d
VsangkyeongV <137258368+VsangkyeongV@users.noreply.github.com>
2023-12-29 22:51:39 +0900

    FEAT:: login페이지 내 이동 구현

--- src/app/_components/components/buttons.tsx
+++ src/app/_components/components/buttons.tsx
@@ -1,7 +1,39 @@
-export const TextButton = ({ text }: { text: string }) => {
+export const TextButton = ({
+  text,
+  className,
+  onClick,
+}: {
+  text: string;
+  className?: string;
+  onClick?: () => void;
+}) => {
   return (
-    <div className="min-h-[26px] min-w-[99px] box-border">
-      <div className="text-gray-400 font-[18px] cursor-pointer flex flex-row justify-center align-center ">
+    <div
+      className={`min-h-[26px] min-w-[99px] box-border ${className}`}
+      onClick={onClick}
+    >
+      <div className="text-gray-400 font-[18px] cursor-pointer flex flex-row justify-center items-center ">
+        {text}
+      </div>
+    </div>
+  );
+};
+
+export const NextButton = ({
+  text,
+  className,
+  onClick,
+}: {
+  text: string;
+  className?: string;
+  onClick?: () => void;
+}) => {
+  return (
+    <div
+      className={`cursor-pointer min-h-[26px] min-w-[99px] w-[404px] box-border bg-gray-200 py-[10px] rounded ${className}`}
+      onClick={onClick}
+    >
+      <div className="font-[18px] cursor-pointer flex flex-row justify-center items-center ">
         {text}
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,176 @@
+"use client";
 import Image from "next/image";
 import Layout from "./_components/layout/Layout";
 import { Input } from "./_components/components/inputbox";
-import { TextButton } from "./_components/components/buttons";
+import { NextButton, TextButton } from "./_components/components/buttons";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <Layout>
-      <div className="h-screen w-screen bg-white">
-        <div className="flex flex-col items-center box-border">
-          <Image
-            src="/icon/logo.png"
-            alt="logo"
-            width={279}
-            height={270}
-            className="mt-[120px]"
-          ></Image>
-          <Input
-            placeholder="Enter loginID"
-            label="아이디(이메일)"
-            className="mt-[105px]"
-          />
-          <Input
-            placeholder="Enter Password"
-            label="패스워드"
-            className="mt-[24px]"
-          />
-          <div className="flex flex-row items-center justify-center mt-[132px] mb-[241px]">
-            <Image src="/image/lock.png" alt="" width={21} height={21}></Image>
-            <TextButton text="아이디 찾기"></TextButton>
-            <TextButton text="비밀번호 찾기"></TextButton>
+  // const [findID, setFindID] = useState(false);
+  const [page, setPage] = useState("returnPW");
+
+  useEffect(() => {
+    window.scrollTo();
+  }, [page]);
+
+  switch (page) {
+    case "":
+      return (
+        // <Layout>
+        <div className="max- h-fit-content bg-white ">
+          <div className="flex flex-col items-center box-border justify-evenly">
+            <Image
+              src="/icon/logo.png"
+              alt="logo"
+              width={100}
+              height={100}
+              className="mt-[30px]"
+            ></Image>
+            <Input
+              placeholder="이메일 주소를 입력해주세요"
+              label="아이디(이메일)"
+            />
+            <Input
+              placeholder="영문, 숫자 포함 8~16자"
+              label="패스워드"
+              className="mt-[24px]"
+            />
+            <div className="flex flex-row items-center justify-center mt-[132px] mb-[241px]">
+              <Image
+                src="/image/lock.png"
+                alt=""
+                width={21}
+                height={21}
+              ></Image>
+              <TextButton
+                text="아이디 찾기"
+                onClick={() => setPage("findID")}
+              ></TextButton>
+              <TextButton
+                text="비밀번호 찾기"
+                onClick={() => setPage("findPW")}
+              ></TextButton>
+            </div>
           </div>
         </div>
-      </div>
-    </Layout>
-  );
+        // </Layout>
+      );
+
+    case "findID":
+      return (
+        <div className="h-screen  bg-white">
+          <div className="h-fit-content items-center flex flex-col justify-evenly box-border">
+            <div className="text-center text-[36px] mt-[100px]">
+              아이디 찾기
+            </div>
+            <Input
+              placeholder=""
+              label="가입하신 이름을 입력해주세요."
+              className="mt-[24px]"
+            />
+            <Input
+              placeholder=""
+              label="가입하신 전화번호를 입력해주세요."
+              className="mt-[24px]"
+            />
+            <NextButton
+              text="다음"
+              className="mt-[64px]"
+              onClick={() => setPage("returnID")}
+            ></NextButton>
+            <div className="flex flex-row items-center justify-center mt-[50px]">
+              <Image
+                src="/image/lock.png"
+                alt=""
+                width={21}
+                height={21}
+                className="mr-[5px]"
+              ></Image>
+
+              <TextButton
+                text="비밀번호를 잊으셨나요?"
+                onClick={() => setPage("findPW")}
+              ></TextButton>
+            </div>
+          </div>
+        </div>
+      );
+
+    case "findPW":
+      return (
+        <div className="h-screen  bg-white">
+          <div className="h-fit-content items-center flex flex-col justify-center box-border">
+            <div className="text-center text-[36px] mt-[100px]">
+              비밀번호 찾기
+            </div>
+            <Input
+              placeholder=""
+              label="가입하신 이름을 입력해주세요."
+              className="mt-[105px]"
+            />
+            <Input
+              placeholder=""
+              label="가입하신 전화번호를 입력해주세요."
+              className="mt-[24px]"
+            />
+            <NextButton
+              text="다음"
+              className="mt-[64px]"
+              onClick={() => setPage("returnPW")}
+            ></NextButton>
+          </div>
+        </div>
+      );
+
+    case "returnID":
+      return (
+        <div className="h-screen  bg-white">
+          <div className="h-fit-content items-center flex flex-col justify-center box-border">
+            <div className="text-center text-[36px] mt-[100px]">
+              아이디 찾기
+            </div>
+            <div className="mt-[102px]">가입하신 아이디를 찾아왔어요!</div>
+            <div className="text-[36px] mt-[30px]">abc8@naver.com</div>
+            <div className="flex flex-row items-center justify-center max-h-[21px] mt-[45px] mb-[67px]">
+              <Image
+                src="/image/lock.png"
+                alt=""
+                width={21}
+                height={21}
+                className="mr-[5px]"
+              ></Image>
+
+              <TextButton
+                text="비밀번호를 잊으셨나요?"
+                onClick={() => setPage("findPW")}
+              ></TextButton>
+            </div>
+            <NextButton
+              text="로그인하러 가기"
+              onClick={() => setPage("")}
+            ></NextButton>
+          </div>
+        </div>
+      );
+
+    case "returnPW":
+      return (
+        <div className="h-screen  bg-white">
+          <div className="h-fit-content items-center flex flex-col justify-center box-border">
+            <div className="text-center text-[36px] mt-[100px]">
+              비밀번호 재설정을 위한 메일을 보냈어요
+            </div>
+            <div className="mt-[102px] mb-[100px] max-w-[420px] text-center">
+              가입하신 이메일로 비밀번호 재설정 링크를 보내드렸어요 링크를 통해
+              비밀번호를 재설정하신 후 로그인 해주세요
+            </div>
+
+            <NextButton
+              text="로그인하러 가기"
+              onClick={() => setPage("")}
+            ></NextButton>
+          </div>
+        </div>
+      );
+  }
 }

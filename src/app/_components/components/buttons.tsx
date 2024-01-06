a6c97cc3f06a319925f32d5ddbb29812569a1516
VsangkyeongV <137258368+VsangkyeongV@users.noreply.github.com>
2024-01-06 14:09:33 +0900

    FIX:: css 수정

--- src/app/_components/components/buttons.tsx
+++ src/app/_components/components/buttons.tsx
@@ -12,7 +12,7 @@ export const TextButton = ({
       className={`min-h-[26px] min-w-[99px] box-border ${className}`}
       onClick={onClick}
     >
-      <div className="text-gray-400 font-[18px] cursor-pointer flex flex-row justify-center items-center ">
+      <div className="text-[#999] font-[18px] cursor-pointer flex flex-row justify-center items-center ">
         {text}
       </div>
     </div>
@@ -23,17 +23,21 @@ export const NextButton = ({
   text,
   className,
   onClick,
+  textColor,
 }: {
   text: string;
   className?: string;
   onClick?: () => void;
+  textColor?: string;
 }) => {
   return (
     <div
-      className={`cursor-pointer min-h-[26px] min-w-[99px] w-[404px] box-border bg-gray-200 py-[10px] rounded ${className}`}
+      className={`cursor-pointer min-h-[26px] min-w-[99px] w-[404px] box-border py-[10px] rounded ${className}`}
       onClick={onClick}
     >
-      <div className="font-[18px] cursor-pointer flex flex-row justify-center items-center ">
+      <div
+        className={`font-[18px] cursor-pointer flex flex-row justify-center items-center ${textColor}`}
+      >
         {text}
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,14 +17,14 @@ export default function Home() {
     case "":
       return (
         // <Layout>
-        <div className="max- h-fit-content bg-white ">
+        <div className="h-screen bg-white flex justify-center items-center">
           <div className="flex flex-col items-center box-border justify-evenly">
             <Image
               src="/icon/logo.png"
               alt="logo"
               width={100}
               height={100}
-              className="mt-[30px]"
+              // className="mt-[30px]"
             ></Image>
             <Input
               placeholder="이메일 주소를 입력해주세요"
@@ -37,15 +37,17 @@ export default function Home() {
             />
             <NextButton
               text="로그인"
-              className="mt-[64px] bg-blue-400"
+              className="mt-[42px] bg-blue-500"
               onClick={() => setPage("returnID")}
+              textColor="text-white"
             ></NextButton>
-            <div className="flex flex-row items-center justify-center mt-[102px] mb-[168px]">
+            <div className="flex flex-row items-center justify-center mt-[41px]">
               <Image
                 src="/image/lock.png"
                 alt=""
                 width={21}
                 height={21}
+                className="mr-[5px]"
               ></Image>
               <TextButton
                 text="아이디 찾기"
@@ -63,15 +65,13 @@ export default function Home() {
 
     case "findID":
       return (
-        <div className="h-screen  bg-white">
-          <div className="h-fit-content items-center flex flex-col justify-evenly box-border">
-            <div className="text-center text-[36px] mt-[100px]">
-              아이디 찾기
-            </div>
+        <div className="h-screen  bg-white flex justify-center items-center">
+          <div className="items-center flex flex-col justify-evenly box-border">
+            <div className="text-center text-[36px]">아이디 찾기</div>
             <Input
               placeholder=""
               label="가입하신 이름을 입력해주세요."
-              className="mt-[24px]"
+              className="mt-[102px]"
             />
             <Input
               placeholder=""
@@ -80,16 +80,16 @@ export default function Home() {
             />
             <NextButton
               text="다음"
-              className="mt-[64px]"
+              className="mt-[64px] bg-gray-200"
               onClick={() => setPage("returnID")}
             ></NextButton>
-            <div className="flex flex-row items-center justify-center mt-[50px]">
+            <div className="flex flex-row items-center justify-center mt-[78px]">
               <Image
                 src="/image/lock.png"
                 alt=""
                 width={21}
                 height={21}
-                className="mr-[5px]"
+                className="mr-[10px]"
               ></Image>
 
               <TextButton
@@ -103,15 +103,13 @@ export default function Home() {
 
     case "findPW":
       return (
-        <div className="h-screen  bg-white">
-          <div className="h-fit-content items-center flex flex-col justify-center box-border">
-            <div className="text-center text-[36px] mt-[100px]">
-              비밀번호 찾기
-            </div>
+        <div className="h-screen  bg-white flex justify-center items-center">
+          <div className="items-center flex flex-col justify-center box-border">
+            <div className="text-center text-[36px]">비밀번호 찾기</div>
             <Input
               placeholder=""
               label="가입하신 이름을 입력해주세요."
-              className="mt-[105px]"
+              className="mt-[102px]"
             />
             <Input
               placeholder=""
@@ -120,7 +118,7 @@ export default function Home() {
             />
             <NextButton
               text="다음"
-              className="mt-[64px]"
+              className="mt-[78px] bg-gray-200"
               onClick={() => setPage("returnPW")}
             ></NextButton>
           </div>
@@ -129,11 +127,9 @@ export default function Home() {
 
     case "returnID":
       return (
-        <div className="h-screen  bg-white">
-          <div className="h-fit-content items-center flex flex-col justify-center box-border">
-            <div className="text-center text-[36px] mt-[100px]">
-              아이디 찾기
-            </div>
+        <div className="h-screen bg-white flex justify-center items-center">
+          <div className="items-center flex flex-col justify-center box-border ">
+            <div className="text-center text-[36px]">아이디 찾기</div>
             <div className="mt-[102px]">가입하신 아이디를 찾아왔어요!</div>
             <div className="text-[36px] mt-[30px]">abc8@naver.com</div>
             <div className="flex flex-row items-center justify-center max-h-[21px] mt-[45px] mb-[67px]">
@@ -153,6 +149,7 @@ export default function Home() {
             <NextButton
               text="로그인하러 가기"
               onClick={() => setPage("")}
+              className="bg-gray-200"
             ></NextButton>
           </div>
         </div>
@@ -160,12 +157,12 @@ export default function Home() {
 
     case "returnPW":
       return (
-        <div className="h-screen  bg-white">
-          <div className="h-fit-content items-center flex flex-col justify-center box-border">
-            <div className="text-center text-[36px] mt-[100px]">
+        <div className="h-screen  bg-white flex justify-center items-center">
+          <div className="items-center flex flex-col justify-center box-border">
+            <div className="text-center text-[36px] ">
               비밀번호 재설정을 위한 메일을 보냈어요
             </div>
-            <div className="mt-[102px] mb-[100px] max-w-[420px] text-center">
+            <div className="mt-[78px] mb-[127px] max-w-[420px] text-center">
               가입하신 이메일로 비밀번호 재설정 링크를 보내드렸어요 링크를 통해
               비밀번호를 재설정하신 후 로그인 해주세요
             </div>
@@ -173,6 +170,7 @@ export default function Home() {
             <NextButton
               text="로그인하러 가기"
               onClick={() => setPage("")}
+              className="bg-gray-200"
             ></NextButton>
           </div>
         </div>
